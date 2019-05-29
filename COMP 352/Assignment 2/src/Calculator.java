52e07b4526b3b0ff7aea32a4e895490e309798c6
neo <anik_patel@live.com>
2019-05-29 15:21:50 -0400

    Added calculation with paranthesis support

--- COMP 352/Assignment 2/src/Calculator.java
+++ COMP 352/Assignment 2/src/Calculator.java
@@ -6,14 +6,50 @@ Due Date: May 29th, 2019
 */
 
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.PrintWriter;
+import java.io.FileNotFoundException;
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Calculator{
-    private static ArrayStack<Integer> values = new ArrayStack<>();
+    private static ArrayStack<Double> values = new ArrayStack<>();
     private static ArrayStack<String> operations = new ArrayStack<>();
-    private static ArrayStack<Integer> st = new ArrayStack<>();
+    public static final int TRUE = 1111111111;
+    public static final int FALSE = 0000000000;
 
-    public static String calculate(int first, int second, String op){
+    /** Local function to get the Precedence level of the operator
+     * 
+     * @param op Operator to check for precedence
+     * @return Integer number with the precedence, higher is more important
+     */
+    private static int getPrecedence(String op){
+        if (op.equals("("))
+            return 0;
+        else if (op.equals("^"))
+            return 6;
+        else if (op.equals("!"))
+            return 5;
+        else if (op.equals("*") || op.equals("/"))
+            return 4;
+        else if (op.equals("+") || op.equals("-"))
+            return 3;
+        else if (op.equals(">") || op.equals("<") || op.equals("<=") || op.equals(">="))
+            return 2;
+        else if (op.equals("==") || op.equals("!="))
+            return 1;
+        return 0;
+    }   
+
+    /** Local function to perform the operation given
+     * 
+     * @param first first number
+     * @param second second number
+     * @param op operation to perform
+     * @return result of the operation
+     */
+    private static String calculate(double first, double second, String op){
         String result = "";
         if (op.equals("^"))
             result += MyMath.power(first, second);
@@ -43,9 +79,139 @@ public class Calculator{
         }
         return result;
     }
+
+    /** Local function to perform an operation from the stack
+     * 
+     * @param op operator to be passed
+     */
+    private static void doOp(String op){
+        double y = values.pop();
+        double x = values.pop();
+        if (op.equals("=")) op = operations.pop() + op;
+
+        String result = calculate(x, y, op);
+        values.push(Double.parseDouble(result));
+    }
+
+    /** Local function to check the precedence of an operator before pushing it to the stack
+     * 
+     * @param current operator to be checked
+     */
+    private static void precedenceCheck(String current){
+        int insidePrecedence, outsidePrecedence;
+
+        if (current.equals("(")){
+            operations.push(current);
+            return;
+        }
+        if (operations.isEmpty()) insidePrecedence = 0;
+        else insidePrecedence = getPrecedence(operations.top());
+
+        outsidePrecedence = getPrecedence(current);
+                    
+        if (insidePrecedence < outsidePrecedence) operations.push(current);
+        else { 
+            if (current.equals(")")){
+                if (operations.top().equals("(")){
+                    operations.pop();
+                    return;
+                } else {
+                    doOp(operations.pop());
+                    precedenceCheck(current);
+                }
+
+            } else if (current.equals("=")){
+                operations.push(current);
+                return;
+            } else {
+            doOp(operations.pop());
+            precedenceCheck(current);
+            }
+        }
+    }
+
+    /** Main Method, has the calculator
+     * 
+     * @param args
+     */
     public static void main(String[] args){
-        
-        System.out.println(calculate(4, 4, "<="));
-        
+        //ArrayList just to read through the file and hold each line
+        ArrayList<String> lineHolder = new ArrayList<>();
+
+        //IO objects
+        Scanner file = null;
+        PrintWriter pw = null;
+
+        System.out.printf("Welcome, welcome!\nThis is, |THE CALCULATOR|\n");
+        System.out.printf("It will calculate all the equations in \"equations.txt\"\n\n");
+
+        try{
+            file = new Scanner(new FileInputStream("equations.txt"));
+            pw = new PrintWriter(new FileOutputStream("out.txt"));   
+            
+            //Reads and stores each line seperately
+            while (file.hasNextLine()){
+                String temp = file.nextLine();
+                lineHolder.add(temp);
+                // System.out.println(temp);
+            }
+
+            //Iterates through each line
+            for (String eq : lineHolder){
+                char[] eqChar = eq.toCharArray();
+                System.out.printf("\nYOUR NEXT LINE WILL BE: %s\n\n", Arrays.toString(eqChar));
+
+                String numHolder = "";
+                //Iterate through each character
+                for (char i : eqChar){
+                    if (i == ' ') continue;
+                    //Converts a character to a String
+                    String holder = String.valueOf(i);
+
+                    //Checks if the character is a number
+                    if (holder.matches("^\\d+$"))
+                        numHolder += holder;
+                    else {
+                        if (!numHolder.equals("")){
+                            values.push(Double.parseDouble(numHolder));
+                            numHolder = "";
+                        }
+                        precedenceCheck(holder);
+                    }
+                }
+                //Pushes the remaining 
+                if (!numHolder.equals(""))
+                    values.push(Double.parseDouble(numHolder));
+                //If there are any operations left, perform them
+                while (!operations.isEmpty())
+                    doOp(operations.pop());
+
+                // Prints out the result of the equations to a file
+                double finalResult = values.pop();
+
+                if (finalResult == TRUE)
+                    pw.printf("The result of %s\n is: \t%b\n", eq, true);
+                else if (finalResult == FALSE)
+                    pw.printf("The result of %s\n is: \t%b\n", eq, false);
+                else
+                    pw.printf("The result of %s\n is: \t%.2f\n", eq, finalResult);
+                
+                // Printing the remaining values from the Stack, for Debugging purposes
+                while (!values.isEmpty()){
+                    System.out.printf("ERROR! THE VALUE STACK STILL HAS %.2f\n", values.pop());
+                }
+                while (!operations.isEmpty()){
+                    System.out.printf("ERROR! THE OPERATIONS STACK STILL HAS %s\n", operations.pop());
+                }
+                pw.printf("\n");
+            }
+        } catch (FileNotFoundException e){
+            System.out.println(e.getMessage());
+
+        } finally {
+            //Closing Scanner and PrintWriter
+            if (file != null) file.close();
+            if (pw != null) pw.close();
+        }
     }
 }
