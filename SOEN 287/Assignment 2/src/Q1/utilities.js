bcad101a1f536350163101e3eb6c5dc4e36f8a67
neo <anik_patel@live.com>
2019-07-25 21:19:24 -0400

    Finished Question 1

--- SOEN 287/Assignment 2/src/Q1/utilities.js
+++ SOEN 287/Assignment 2/src/Q1/utilities.js
@@ -1,7 +1,39 @@
-function validateNumber() {}
+// var number;
 
-function isPrimeNumber() {}
+function validateNumber() {
+  let number = document.getElementById("input-number").value;
+  if (isNaN(number)) {
+    alert("Please enter a numeric value!");
+    number = null;
+  }
+  return number;
+}
 
-function reverseNumber() {}
+function isPrimeNumber() {
+  let number = validateNumber();
+  if (!number) {
+    //Checking if previous failed or not
+    return;
+  }
+  let isPrime = true;
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      isPrime = false;
+    }
+  }
+  if (isPrime) {
+    let reverse = reverseNumber(number);
+    alert(`This is a Prime and the Reverse is: ${reverse}`);
+  } else {
+    alert("This is not a Prime!");
+  }
+}
 
-function buttonCheck() {}
+function reverseNumber(number) {
+  let temp = number.toString();
+  let reversed = [];
+  for (let i = 0; i < temp.length; i++) {
+    reversed.unshift(temp.charAt(i));
+  }
+  return reversed.join("");
+}
