a081162cddf9bb2cf296db7ae6f1bdea781f3072
neojinxd <anik_patel@live.com>
2020-03-11 02:51:26 -0400

    Updated shaders to handle texture

--- COMP 371/Assignment 2/VS2017/assets/shaders/fragShader.glsl
+++ COMP 371/Assignment 2/VS2017/assets/shaders/fragShader.glsl
@@ -1,10 +1,15 @@
 #version 330 core
 in vec3 vertexColor;
+in vec2 vertexUV;
+
 out vec4 FragColor;
 
 uniform vec4 uColor;
+uniform sampler2D textureSampler;
+
 // vec4(vertexColor.r, vertexColor.g, vertexColor.b, 1.0f)
 void main()
 {
-   FragColor = uColor;
+   vec4 textureColor = texture( textureSampler, vertexUV );
+   FragColor = textureColor * vec4(uColor.r, uColor.g, uColor.b, uColor.a);
 }
--- COMP 371/Assignment 2/VS2017/assets/shaders/vertexShader.glsl
+++ COMP 371/Assignment 2/VS2017/assets/shaders/vertexShader.glsl
@@ -2,15 +2,18 @@
 
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec3 aColor;
+layout (location = 2) in vec2 aUv;
 
 uniform mat4 worldMatrix = mat4(1.0);
 uniform mat4 viewMatrix = mat4(1.0);
 uniform mat4 projectionMatrix = mat4(1.0);
 
 out vec3 vertexColor;
+out vec2 vertexUV;
 
 void main()
 {
    vertexColor = aColor;
+   vertexUV = aUv;
    gl_Position = projectionMatrix * viewMatrix * worldMatrix * vec4(aPos.x, aPos.y, aPos.z, 1.0);
 }
