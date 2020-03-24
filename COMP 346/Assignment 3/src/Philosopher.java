2df102a718964c3aca337a736265d40124dd8cac
neoJINXD <anik_patel@live.com>
2020-03-23 22:14:12 -0400

    Implemented Philosopher class

--- COMP 346/Assignment 3/src/Philosopher.java
+++ COMP 346/Assignment 3/src/Philosopher.java
@@ -1,36 +1,31 @@
+import java.util.Random;
+
 import common.BaseThread;
 
 /**
- * Class Philosopher.
- * Outlines main subrutines of our virtual philosopher.
+ * Class Philosopher. Outlines main subrutines of our virtual philosopher.
  *
  * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
  */
-public class Philosopher extends BaseThread
-{
+public class Philosopher extends BaseThread {
 	/**
 	 * Max time an action can take (in milliseconds)
 	 */
 	public static final long TIME_TO_WASTE = 1000;
 
 	/**
-	 * The act of eating.
-	 * - Print the fact that a given phil (their TID) has started eating.
-	 * - yield
-	 * - Then sleep() for a random interval.
-	 * - yield
-	 * - The print that they are done eating.
+	 * The act of eating. - Print the fact that a given phil (their TID) has started
+	 * eating. - yield - Then sleep() for a random interval. - yield - The print
+	 * that they are done eating.
 	 */
-	public void eat()
-	{
-		try
-		{
-			// ...
-			sleep((long)(Math.random() * TIME_TO_WASTE));
-			// ...
-		}
-		catch(InterruptedException e)
-		{
+	public void eat() {
+		try {
+			System.out.printf("The time is to consume💦\n");
+			yield();
+			sleep((long) (Math.random() * TIME_TO_WASTE));
+			yield();
+			System.out.printf("The consuming is completing\n");
+		} catch (InterruptedException e) {
 			System.err.println("Philosopher.eat():");
 			DiningPhilosophers.reportException(e);
 			System.exit(1);
@@ -38,42 +33,44 @@ public class Philosopher extends BaseThread
 	}
 
 	/**
-	 * The act of thinking.
-	 * - Print the fact that a given phil (their TID) has started thinking.
-	 * - yield
-	 * - Then sleep() for a random interval.
-	 * - yield
-	 * - The print that they are done thinking.
+	 * The act of thinking. - Print the fact that a given phil (their TID) has
+	 * started thinking. - yield - Then sleep() for a random interval. - yield - The
+	 * print that they are done thinking.
 	 */
-	public void think()
-	{
-		// ...
+	public void think() {
+		try {
+			System.out.printf("The time is 🤔\n");
+			yield();
+			sleep((long) (Math.random() * TIME_TO_WASTE));
+			yield();
+			System.out.printf("No more thonking\n");
+		} catch (InterruptedException e) {
+			System.err.println("Philosopher.think():");
+			DiningPhilosophers.reportException(e);
+			System.exit(1);
+		}
 	}
 
 	/**
-	 * The act of talking.
-	 * - Print the fact that a given phil (their TID) has started talking.
-	 * - yield
-	 * - Say something brilliant at random
-	 * - yield
-	 * - The print that they are done talking.
+	 * The act of talking. - Print the fact that a given phil (their TID) has
+	 * started talking. - yield - Say something brilliant at random - yield - The
+	 * print that they are done talking.
 	 */
-	public void talk()
-	{
-		// ...
+	public void talk() {
 
+		System.out.printf("The time is speech👁‍🗨\n");
+		yield();
 		saySomething();
+		yield();
+		System.out.printf("Silence\n");
 
-		// ...
 	}
 
 	/**
 	 * No, this is not the act of running, just the overridden Thread.run()
 	 */
-	public void run()
-	{
-		for(int i = 0; i < DiningPhilosophers.DINING_STEPS; i++)
-		{
+	public void run() {
+		for (int i = 0; i < DiningPhilosophers.DINING_STEPS; i++) {
 			DiningPhilosophers.soMonitor.pickUp(getTID());
 
 			eat();
@@ -83,12 +80,12 @@ public class Philosopher extends BaseThread
 			think();
 
 			/*
-			 * TODO:
-			 * A decision is made at random whether this particular
-			 * philosopher is about to say something terribly useful.
+			 * TODO: A decision is made at random whether this particular philosopher is
+			 * about to say something terribly useful.
 			 */
-			if(true == false)
-			{
+			Random rand = new Random();
+			// if(rand.nextInt(10) < 5)
+			if (true) {
 				// Some monitor ops down here...
 				talk();
 				// ...
@@ -99,25 +96,17 @@ public class Philosopher extends BaseThread
 	} // run()
 
 	/**
-	 * Prints out a phrase from the array of phrases at random.
-	 * Feel free to add your own phrases.
+	 * Prints out a phrase from the array of phrases at random. Feel free to add
+	 * your own phrases.
 	 */
-	public void saySomething()
-	{
-		String[] astrPhrases =
-		{
-			"Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
-			"You know, true is false and false is true if you think of it",
-			"2 + 2 = 5 for extremely large values of 2...",
-			"If thee cannot speak, thee must be silent",
-			"My number is " + getTID() + ""
-		};
+	public void saySomething() {
+		String[] astrPhrases = { "Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
+				"You know, true is false and false is true if you think of it",
+				"2 + 2 = 5 for extremely large values of 2...", "If thee cannot speak, thee must be silent",
+				"My number is " + getTID() + "" };
 
-		System.out.println
-		(
-			"Philosopher " + getTID() + " says: " +
-			astrPhrases[(int)(Math.random() * astrPhrases.length)]
-		);
+		System.out.println(
+				"Philosopher " + getTID() + " says: " + astrPhrases[(int) (Math.random() * astrPhrases.length)]);
 	}
 }
 
